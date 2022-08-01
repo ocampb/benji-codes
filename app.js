a9bc32383a863dbfa05104cac34341373a1578aa
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-07-31 20:45:55 -0400

    authentication, login flow and password encryption

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 require("dotenv").config();
 var createError = require("http-errors");
+const session = require("express-session");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var PORT = process.env.PORT || 3000;
 const es6Renderer = require("express-es6-template-engine");
-
+const usersRoutes = require("./routes/users");
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+
+const isAuthenticated = (req, res, next) => {
+  if (req.session.userId) {
+    console.log("Logged in");
+    next();
+  } else {
+    console.log("NOT LOGGED IN");
+    res.redirect("/login.html");
+  }
+};
 
 var app = express();
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -23,7 +42,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/users", isAuthenticated, usersRoutes);
 app.use("/", indexRouter);
+
 //app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
@@ -36,6 +57,8 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  console.log(err.message);
 
   // render the error page
   res.status(err.status || 500);
---
+++ routes/users/index.js
@@ -0,0 +1,52 @@
+const { Users } = require("../../models");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+router.post("/create_user", async (req, res) => {
+  const { name, username, password } = req.body;
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const encryptedUser = {
+      name: name,
+      username: username,
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    console.log(encryptedUser);
+    const createUser = await Users.create(encryptedUser);
+    console.log(createUser);
+    res.send(createUser);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const databaseentry = await Users.findOne({ where: { username: username } });
+  console.log(databaseentry);
+  const passwordCorrect = await bcrypt.compare(
+    password,
+    databaseentry.password
+  );
+  if (passwordCorrect === true) {
+    console.log("PASSWORD IS CORRECT");
+    req.session.userId = databaseentry.id;
+
+    res.redirect("/");
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/test", (req, res) => {
+  console.log(req.session.userId);
+
+  res.send("test");
+});
+
+module.exports = router;
