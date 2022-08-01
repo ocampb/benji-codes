6921b20741703d7bd3c810d93b990134f97462d2
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-08-01 15:14:58 -0400

    Update index.js

--- routes/veterinary_visits/index.js
+++ routes/veterinary_visits/index.js
@@ -2,6 +2,27 @@ const { Veterinary_Visits } = require("../../models");
 const express = require("express");
 const router = express.Router();
 
+router.get("/view_vet_visit", async (req, res) => {
+  const { vet_name, date_of_visit, reason, notes, PetId } = req.body;
+  try {
+    const Vet_Visit = {
+      vet_name: vet_name,
+      date_of_visit: date_of_visit,
+      reason: reason,
+      notes: notes,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      PetId: PetId,
+    };
+    const Vet_Visit1 = await Veterinary_Visits.create(Vet_Visit);
+    console.log(Vet_Visit1);
+    res.send(Vet_Visit1);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
 router.post("/create_vet_visit", async (req, res) => {
   const { vet_name, date_of_visit, reason, notes, PetId } = req.body;
   try {
