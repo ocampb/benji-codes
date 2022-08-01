8976b18080de8704501ca33ea0f8a2b138211a0c
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-08-01 19:20:26 -0400

    routes + updated login

--- routes/enrichment/index.js
+++ routes/enrichment/index.js
@@ -23,4 +23,70 @@ router.post("/create_enrichment", async (req, res) => {
   }
 });
 
+router.get("/view_enrichment_by_id/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const Enrichment1 = await Enrichment.findOne({
+      where: { id: id },
+    });
+
+    console.log(Enrichment1);
+    res.send(Enrichment1);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.get("/view_all_enrichment_by_petid/:petid", async (req, res) => {
+  const { petid } = req.params;
+  try {
+    const Enrichment1 = await Enrichment.findAll({
+      where: { PetId: petid },
+    });
+    console.log(Enrichment1);
+    res.send(Enrichment1);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// works but throws a lot of errors and crashes nodemon
+router.delete("/delete_by_id/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const Enrichment1 = await Enrichment.destroy({
+      where: { id: id },
+    });
+    console.log(Enrichment1);
+    res.status(200).end();
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.put("/update_by_id/:id", async (req, res) => {
+  const { id } = req.params;
+  const { type, duration, notes } = req.body;
+  try {
+    const updatedRows = await Enrichment.update(
+      {
+        type: type,
+        duration: duration,
+        notes: notes,
+      },
+      {
+        where: { id: id },
+      }
+    );
+    console.log(updatedRows);
+    res.send(updatedRows);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
 module.exports = router;
